d674d35d17ae0b5a6dc2c6ca1e481bffa94590a3
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-21 11:23:15 +0500

    bump

--- Sources/PrepFoodForm/ImageViewer/Scanner/ScannerInput.swift
+++ Sources/PrepFoodForm/ImageViewer/Scanner/ScannerInput.swift
@@ -1274,6 +1274,12 @@ import VisionSugar
 extension RecognizedText {
     func allDetectedFoodLabelValues(for attribute: Attribute) -> [FoodLabelValue] {
         var allValues: [FoodLabelValue] = []
+        
+        func addValueIfNotExisting(_ value: FoodLabelValue) {
+            guard !allValues.contains(value) else { return }
+            allValues.append(value)
+        }
+        
         for candidate in candidates {
             let detectedValues = candidate.detectedValues
             for value in detectedValues {
@@ -1286,9 +1292,12 @@ extension RecognizedText {
                     valueWithUnit = value
                 }
                 
-                /// Make sure we're not returning duplicate values
-                guard !allValues.contains(valueWithUnit) else { continue }
-                allValues.append(value)
+                addValueIfNotExisting(valueWithUnit)
+                
+                if attribute == .energy {
+                    let oppositeUnit: FoodLabelUnit = valueWithUnit.unit == .kcal ? .kj : .kcal
+                    addValueIfNotExisting(FoodLabelValue(amount: valueWithUnit.amount, unit: oppositeUnit))
+                }
             }
         }
         return allValues
