51abbe5dcaa73ea22d920c065762fe4d78eb9297
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-21 11:27:28 +0500

    bump

--- Sources/PrepFoodForm/ImageViewer/Scanner/ScannerInput.swift
+++ Sources/PrepFoodForm/ImageViewer/Scanner/ScannerInput.swift
@@ -523,11 +523,11 @@ public struct ScannerInput: View {
         }
         
         var valueSuggestions: [FoodLabelValue] {
-//            [.init(amount: 320, unit: .kcal), .init(amount: 320, unit: .kj), .init(amount: 320), .init(amount: 3200, unit: .kcal), .init(amount: 3200, unit: .kj)]
-            guard let text = viewModel.currentValueText, let attribute = viewModel.currentAttribute else {
-                return []
-            }
-            return text.allDetectedFoodLabelValues(for: attribute)
+            [.init(amount: 320, unit: .kcal), .init(amount: 320, unit: .kj), .init(amount: 320), .init(amount: 3200, unit: .kcal), .init(amount: 3200, unit: .kj)]
+//            guard let text = viewModel.currentValueText, let attribute = viewModel.currentAttribute else {
+//                return []
+//            }
+//            return text.allDetectedFoodLabelValues(for: attribute)
         }
         
         var suggestionsLayer: some View {
@@ -537,8 +537,8 @@ public struct ScannerInput: View {
             }
             
             var textColor: Color {
-                .primary
-//                .secondary
+//                .primary
+                .secondary
             }
             
             return ScrollView(.horizontal, showsIndicators: false) {
@@ -551,7 +551,7 @@ public struct ScannerInput: View {
                             .frame(height: SuggestionsBarHeight)
                             .background(
 //                                Capsule(style: .continuous)
-                                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                RoundedRectangle(cornerRadius: 5, style: .continuous)
                                     .foregroundColor(backgroundColor)
                             )
                     }
