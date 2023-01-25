e9fe8a34bdb8be3fa2b48ba82996461bdd59163e
Ahmed Khalaf <pxlshpr@gmail.com>
2023-01-25 17:41:00 +0500

    bump

--- Sources/PrepFoodForm/_To Sort/_To sort.swift
+++ Sources/PrepFoodForm/_To Sort/_To sort.swift
@@ -547,27 +547,6 @@ extension Field {
 }
 
 extension ScanResult {
-    var headerTitle1_legacy: String {
-        guard let headerType = headers?.header1Type else { return "Column 1" }
-        return headerType.description.replacingFirstOccurrence(of: "Per ", with: "")
-    }
-    var headerTitle2_legacy: String {
-        guard let headerType = headers?.header2Type else { return "Column 2" }
-        return headerType.description.replacingFirstOccurrence(of: "Per ", with: "")
-    }
-    
-    var headerTitle1: String {
-        guard let headerType = headers?.header1Type else { return "Column 1" }
-        return headerType.description
-    }
-    var headerTitle2: String {
-        guard let headerType = headers?.header2Type else { return "Column 2" }
-        return headerType.description
-    }
-
-}
-
-extension ScanResult {
     var dataPointsCount: Int {
         var count = nutrientsCount
         if serving?.amount != nil {
