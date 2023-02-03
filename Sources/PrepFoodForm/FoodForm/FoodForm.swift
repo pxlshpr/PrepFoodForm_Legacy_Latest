20363f54c4d36271a786ba8d158f6e0eb63c6a73
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-03 23:28:53 +0500

    tag increment

--- Sources/PrepFoodForm/FoodForm/FoodForm.swift
+++ Sources/PrepFoodForm/FoodForm/FoodForm.swift
@@ -795,37 +795,3 @@ extension FoodForm.Fields {
         && prefilledFood == nil
     }
 }
-
-extension View {
-    func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
-        background(
-            GeometryReader { geometryProxy in
-                Color.clear
-                    .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
-            }
-        )
-        .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
-    }
-}
-
-private struct SizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
-}
-
-extension View {
-    func readSafeAreaInsets(onChange: @escaping (EdgeInsets) -> Void) -> some View {
-        background(
-            GeometryReader { geometryProxy in
-                Color.clear
-                    .preference(key: InsetsPreferenceKey.self, value: geometryProxy.safeAreaInsets)
-            }
-        )
-        .onPreferenceChange(InsetsPreferenceKey.self, perform: onChange)
-    }
-}
-
-private struct InsetsPreferenceKey: PreferenceKey {
-    static var defaultValue: EdgeInsets = .init()
-    static func reduce(value: inout EdgeInsets, nextValue: () -> EdgeInsets) {}
-}
