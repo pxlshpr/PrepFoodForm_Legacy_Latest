877b000405dbeebcf9f1bf60343480969dd16945
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-09 09:33:33 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pxlshpr/EmojiPicker", from: "0.0.22"),
-        .package(url: "https://github.com/pxlshpr/FoodLabel", from: "0.0.35"),
+        .package(url: "https://github.com/pxlshpr/FoodLabel", from: "0.0.36"),
 //        .package(url: "https://github.com/pxlshpr/FoodLabelCamera", from: "0.0.43"),
         .package(url: "https://github.com/pxlshpr/FoodLabelExtractor", from: "0.0.30"),
         .package(url: "https://github.com/pxlshpr/FoodLabelScanner", from: "0.0.148"),
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/pxlshpr/NamePicker", from: "0.0.20"),
         .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.244"),
         .package(url: "https://github.com/pxlshpr/PrepNetworkController", from: "0.0.22"),
-        .package(url: "https://github.com/pxlshpr/PrepViews", from: "0.0.136"),
+        .package(url: "https://github.com/pxlshpr/PrepViews", from: "0.0.142"),
         .package(url: "https://github.com/pxlshpr/SwiftHaptics", from: "0.1.3"),
         .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.86"),
         .package(url: "https://github.com/pxlshpr/SwiftUICamera", from: "0.0.41"),
--- Sources/PrepFoodForm/FoodForm/FoodForm+Views.swift
+++ Sources/PrepFoodForm/FoodForm/FoodForm+Views.swift
@@ -5,36 +5,6 @@ import PrepViews
 
 extension FoodForm {
     var foodLabel: FoodLabel {
-//        let energyBinding = Binding<FoodLabelValue>(
-//            get: { fields.energy.value.value ?? .init(amount: 0, unit: .kcal) },
-//            set: { _ in }
-//        )
-//
-//        let carbBinding = Binding<Double>(
-//            get: { fields.carb.value.double ?? 0 },
-//            set: { _ in }
-//        )
-//
-//        let fatBinding = Binding<Double>(
-//            get: { fields.fat.value.double ?? 0 },
-//            set: { _ in }
-//        )
-//
-//        let proteinBinding = Binding<Double>(
-//            get: { fields.protein.value.double ?? 0 },
-//            set: { _ in }
-//        )
-//
-//        let microsBinding = Binding<[NutrientType : FoodLabelValue]>(
-//            get: { fields.microsDict },
-//            set: { _ in }
-//        )
-//
-//        let amountBinding = Binding<String>(
-//            get: { fields.amount.doubleValueDescription },
-//            set: { _ in }
-//        )
-        
         let dataBinding = Binding<FoodLabelData>(
             get: {
                 FoodLabelData(
@@ -50,15 +20,7 @@ extension FoodForm {
             set: { _ in }
         )
 
-        return FoodLabel(
-            data: dataBinding
-//            energyValue: energyBinding,
-//            carb: carbBinding,
-//            fat: fatBinding,
-//            protein: proteinBinding,
-//            nutrients: microsBinding,
-//            amountPerString: amountBinding
-        )
+        return FoodLabel(data: dataBinding)
     }
     
     var servingsAndSizesCell: some View {
